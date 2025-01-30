b2f21c3016e1fed17486a1f602cbd778ae3a21a7
davila1292 <daap1292@gmail.com>
2025-01-29 20:28:28 -0600

    Update DemoTest3.java

--- src/demo1/DemoTest3.java
+++ src/demo1/DemoTest3.java
@@ -28,42 +28,7 @@ public class DemoTest3 {
 			team3= "Iran";
 			team4= "Wales";
 			break;
-		case 'C':
-			team1= "Argentina";
-			team2= "Poland";
-			team3= "Mexico";
-			team4= "Saudi Arabia";
-			break;
-		case 'D':
-			team1= "France";
-			team2= "Australia";
-			team3= "Tunisia";
-			team4= "Denmark";
-			break;
-		case 'E':
-			team1= "Japan";
-			team2= "Spain";
-			team3= "Germany";
-			team4= "Costa Rica";
-			break;
-		case 'F':
-			team1= "Morocco";
-			team2= "Croatia";
-			team3= "Belgium";
-			team4= "Canada";
-			break;
-		case 'G':
-			team1= "Brazil";
-			team2= "Switzerland";
-			team3= "Cameroon";
-			team4= "Serbia";
-			break;
-		case 'H':
-			team1= "Portugal";
-			team2= "South Korea";
-			team3= "Uruguay";
-			team4= "Ghana";
-			break;
+
 		default:
 			message = "WRONG GROUP";
 			isGroupExist = false;
